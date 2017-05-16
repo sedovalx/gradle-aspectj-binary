ddc6c9ae8cc7229cc2cb9d930fd540ea4a418577
Alexander Sedov <alexander.sedov@jetbrains.com>
2017-05-16 21:48:18 +0300

    Fixed the java example

--- examples/src/main/java/com/github/sedovalx/sandbox/gradle/aspectj/example/aspects/JavaAspect.java
+++ examples/src/main/java/com/github/sedovalx/sandbox/gradle/aspectj/example/aspects/JavaAspect.java
@@ -1,14 +1,12 @@
 package com.github.sedovalx.sandbox.gradle.aspectj.example.aspects;
 
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class JavaAspect {
     @Before("@annotation(annotation) && execution(* *(..))")
-    public Object before(ProceedingJoinPoint joinPoint, JavaAnnotation annotation) throws Throwable {
+    public void before(JavaAnnotation annotation) throws Throwable {
         System.out.println("Running from " + annotation.value() + " before the execution");
-        return joinPoint.proceed();
     }
 }
