d47d163360c8bcac6477a02e1a5d54158b6f08da
Alexander Sedov <alexander.sedov@jetbrains.com>
2019-09-12 15:28:47 +0300

    Skip travis install step to avoid premature calling of `gradle assemble`

--- travis.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo "include 'plugin'" > settings.gradle
-./gradlew clean :plugin:publishMavenJavaPublicationToMavenLocal
-echo "include 'plugin', 'examples'" > settings.gradle
-./gradlew :examples:run
-./gradlew bintrayUpload
